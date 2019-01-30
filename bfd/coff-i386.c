a2bbc04cf19d6025d5cbef7d2a838f060f5ad55a
Hannes Domani <ssbssa@yahoo.de>
2019-01-31 00:48:50 +0100

    Read minidump file.

--- bfd/coff-i386.c
+++ bfd/coff-i386.c
@@ -672,7 +672,7 @@ const bfd_target
     _bfd_dummy_target,
     COFF_CHECK_FORMAT,
     bfd_generic_archive_p,
-    COFF_CHECK_FORMAT
+    coff_core_file_p
   },
   {				/* bfd_set_format */
     _bfd_bool_bfd_false_error,
@@ -743,7 +743,7 @@ const bfd_target
     _bfd_dummy_target,
     COFF_CHECK_FORMAT,
     bfd_generic_archive_p,
-    COFF_CHECK_FORMAT
+    coff_core_file_p
   },
   {				/* bfd_set_format */
     _bfd_bool_bfd_false_error,
