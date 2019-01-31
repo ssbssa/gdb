45122fafd5fa88a78e90cf2f78c6ceebfde2e953
Hannes Domani <ssbssa@yahoo.de>
2019-01-31 17:13:12 +0100

    Use TEB pointer of minidump file for $_tlb.

--- gdb/corelow.c
+++ gdb/corelow.c
@@ -99,6 +99,8 @@ public:
 
   bool info_proc (const char *, enum info_proc_what) override;
 
+  bool get_tib_address (ptid_t ptid, CORE_ADDR *addr) override;
+
   /* A few helpers.  */
 
   /* Getter, see variable definition.  */
@@ -1106,6 +1108,24 @@ bool
 core_target::has_registers ()
 {
   return (core_bfd != NULL);
+}
+
+bool
+core_target::get_tib_address (ptid_t ptid, CORE_ADDR *addr)
+{
+  char secname[32];
+  struct bfd_section *section;
+
+  sprintf (secname, ".coretlb/%ld", ptid.lwp ());
+
+  section = bfd_get_section_by_name (core_bfd, secname);
+  if (section == NULL)
+    return false;
+
+  if (bfd_section_size (section) < sizeof addr)
+    return false;
+
+  return bfd_get_section_contents (core_bfd, section, addr, 0, sizeof addr);
 }
 
 /* Implement the to_info_proc method.  */
