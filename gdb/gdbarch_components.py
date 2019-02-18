eaf4f6793c00e85b0e324a615dce942ed233bf31
Hannes Domani <ssbssa@yahoo.de>
2019-02-18 18:07:16 +0100

    Use executable path of corefile as program to be debugged

--- gdb/gdbarch_components.py
+++ gdb/gdbarch_components.py
@@ -1784,6 +1784,17 @@ Returns true if the layout was read successfully.
     predicate=True,
 )
 
+Method(
+    comment="""
+Return the executable path of a core file.
+""",
+    type="char *",
+    name="core_load_executable",
+    params=[],
+    predicate=True,
+    invalid=True,
+)
+
 Value(
     comment="""
 BFD target to use when generating a core file.
