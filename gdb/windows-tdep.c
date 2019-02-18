7e5cda24298afe0e4acb959d732b6ff009f07a2d
Hannes Domani <ssbssa@yahoo.de>
2019-02-18 18:29:32 +0100

    Use buildid with symbol server.

--- gdb/windows-tdep.c
+++ gdb/windows-tdep.c
@@ -695,7 +695,7 @@ symbol_server_free (void);
 
 const char *
 symbol_server_lib (const char *orig_lib_name,
-		   uint32_t size, uint32_t timestamp);
+		   uint32_t size, uint32_t timestamp, uint32_t *buildid);
 #endif
 
 struct cpms_data
@@ -732,6 +732,10 @@ core_get_module_name (struct gdbarch *gdbarch, const char *sect_name,
       uint32_t size = 0;
       uint32_t timestamp = 0;
       const char *symlib;
+      unsigned long long base_addr;
+      uint32_t buildid[5];
+      char name[32];
+      uint32_t *b = NULL;
 
       findstr = strstr (sect_name, ";s=");
       if (findstr)
@@ -742,7 +746,16 @@ core_get_module_name (struct gdbarch *gdbarch, const char *sect_name,
 
       findstr = strstr (sect_name, ";v=");
 
-      symlib = symbol_server_lib (module_name, size, timestamp);
+      sscanf (sect_name + 12, "%llx", &base_addr);
+
+      sprintf (name, ".corebuildid/%llx", base_addr);
+      asection *s = bfd_get_section_by_name (core_bfd, name);
+
+      if (s && bfd_get_section_size (s) == 20
+	  && bfd_get_section_contents (core_bfd, s, buildid, 0, 20))
+	b = buildid;
+
+      symlib = symbol_server_lib (module_name, size, timestamp, b);
       if (symlib)
 	module_name = symlib;
       else if (findstr)
