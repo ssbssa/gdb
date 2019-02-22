d9ec15185482886e08e74dacdbe91209493a2946
Hannes Domani <ssbssa@yahoo.de>
2019-02-22 18:26:06 +0100

    Create/enable/disable/delete breakpoints in TUI with mouse.

--- gdb/tui/tui-source.h
+++ gdb/tui/tui-source.h
@@ -56,6 +56,8 @@ struct tui_source_window : public tui_source_window_base
   void display_start_addr (struct gdbarch **gdbarch_p,
 			   CORE_ADDR *addr_p) override;
 
+  void click (int mouse_x, int mouse_y, int mouse_button) override;
+
 protected:
 
   void do_scroll_vertical (int num_to_scroll) override;
