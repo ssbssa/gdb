9b6bf491dd7222f588fece2a7a7eff8b5df7c569
Hannes Domani <ssbssa@yahoo.de>
2019-03-18 19:07:47 +0100

    Disable source styling if source-highlight configuration files are missing

--- gdb/cli/cli-style.c
+++ gdb/cli/cli-style.c
@@ -376,7 +376,16 @@ If enabled, output to the terminal is styled."),
 			   set_style_enabled, show_style_enabled,
 			   &style_set_list, &style_show_list);
 
-  add_setshow_boolean_cmd ("sources", no_class, &source_styling, _("\
+#ifdef USE_RELATIVE_SRC_HIGHLIGHT
+  std::string esc_style = gdb_datadir;
+  esc_style += "/../source-highlight/esc.style";
+  struct stat st;
+  if (stat (esc_style.c_str (), &st) != 0)
+    source_styling = 0;
+  else
+#endif
+    {
+      add_setshow_boolean_cmd ("sources", no_class, &source_styling, _("\
 Set whether source code styling is enabled."), _("\
 Show whether source code styling is enabled."), _("\
 If enabled, source code is styled.\n"
@@ -388,8 +397,9 @@ see \"show style enabled\"."
 it was not linked against GNU Source Highlight.  However, it might still be\n\
 available if the appropriate extension is available at runtime."
 #endif
-			   ), set_style_enabled, show_style_sources,
-			   &style_set_list, &style_show_list);
+			       ), set_style_enabled, show_style_sources,
+			       &style_set_list, &style_show_list);
+    }
 
   add_setshow_prefix_cmd ("disassembler", no_class,
 			  _("\
