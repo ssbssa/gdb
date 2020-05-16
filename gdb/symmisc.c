3f07379215dd9c4d40da92a5e18aab04d3f84c1b
Hannes Domani <ssbssa@yahoo.de>
2020-05-16 18:16:42 +0200

    Add column information to maint info line-table

--- gdb/symmisc.c
+++ gdb/symmisc.c
@@ -984,6 +984,7 @@ maintenance_print_one_line_table (struct symtab *symtab, void *data)
       ui_out_emit_table table_emitter (uiout, 7, -1, "line-table");
       uiout->table_header (6, ui_left, "index", _("INDEX"));
       uiout->table_header (6, ui_left, "line", _("LINE"));
+      uiout->table_header (6, ui_left, "column", _("COLUMN"));
       uiout->table_header (18, ui_left, "rel-address", _("REL-ADDRESS"));
       uiout->table_header (18, ui_left, "unrel-address", _("UNREL-ADDRESS"));
       uiout->table_header (7, ui_left, "is-stmt", _("IS-STMT"));
@@ -1003,6 +1004,10 @@ maintenance_print_one_line_table (struct symtab *symtab, void *data)
 				 line_number_style.style ());
 	  else
 	    uiout->field_string ("line", _("END"));
+	  if (item->column > 0)
+	    uiout->field_signed ("column", item->column);
+	  else
+	    uiout->field_string ("column", "");
 	  uiout->field_core_addr ("rel-address", objfile->arch (),
 				  item->pc (objfile));
 	  uiout->field_core_addr ("unrel-address", objfile->arch (),
