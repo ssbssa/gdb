58ef639c4048f585c7e9227a6a5f4bfcced1e5ee
Hannes Domani <ssbssa@yahoo.de>
2020-05-16 17:40:28 +0200

    Add column information of dwarf to the symbol information
    ---
    v2:
    - Don't coalesce line table entries with different column numbers.
    - Move column number argument right after line number argument.

--- gdb/frame.c
+++ gdb/frame.c
@@ -2894,6 +2894,7 @@ find_frame_sal (const frame_info_ptr &frame)
 	{
 	  sal.symtab = sym->symtab ();
 	  sal.line = sym->line ();
+	  sal.column = sym->column ();
 	}
       else
 	/* If the symbol does not have a location, we don't know where
