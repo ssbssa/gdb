ec19658228641b476266f5a18746e047d7621893
Hannes Domani <ssbssa@yahoo.de>
2020-09-27 16:40:12 +0200

    Use executable base of minidump file

--- bfd/coffgen.c
+++ bfd/coffgen.c
@@ -3453,6 +3453,17 @@ coff_core_file_p (bfd *abfd)
 	  != sizeof moduleCount)
 	goto fail;
 
+      if (moduleCount > 0)
+	{
+	  sec = make_bfd_asection (abfd, ".corebase",
+				   SEC_HAS_CONTENTS,
+				   moduleListRva + 4,
+				   8,
+				   0);
+	  if (!sec)
+	    goto fail;
+	}
+
       for (m = 0; m < moduleCount; m++)
 	{
 	  if (bfd_bread (&module, sizeof module, abfd) != sizeof module)
