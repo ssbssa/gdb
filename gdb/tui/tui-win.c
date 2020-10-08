9baf8095fde56e208b9c650d8d7c405544035bf3
Hannes Domani <ssbssa@yahoo.de>
2020-10-08 14:50:06 +0200

    Always call resize_term on Windows to remove the scrollbar

--- gdb/tui/tui-win.c
+++ gdb/tui/tui-win.c
@@ -503,8 +503,12 @@ tui_resize_all (void)
   rl_get_screen_size (&screenheight, &screenwidth);
   screenwidth += readline_hidden_cols;
 
+#ifndef _WIN32
   width_diff = screenwidth - tui_term_width ();
   height_diff = screenheight - tui_term_height ();
+#else
+  width_diff = height_diff = 1;
+#endif
   if (height_diff || width_diff)
     {
 #ifdef HAVE_RESIZE_TERM
