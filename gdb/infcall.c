eda0e5b3b847cf9689933465e5adc35b4e2702a4
Simon Marchi <simon.marchi@efficios.com>
2021-08-12 16:07:55 -0400

    Refresh stale frame_info object
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=28224

--- gdb/infcall.c
+++ gdb/infcall.c
@@ -817,6 +817,8 @@ call_function_by_hand_dummy (struct value *function,
 	     "target calling convention."),
 	   get_function_name (funaddr, name_buf, sizeof (name_buf)));
 
+  frame = get_current_frame ();
+
   if (values_type == NULL)
     values_type = default_return_type;
   if (values_type == NULL)
