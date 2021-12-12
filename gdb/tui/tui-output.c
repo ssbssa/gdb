ced1ac3901b6943f9b99191bb54fe6809ede8c58
Hannes Domani <ssbssa@yahoo.de>
2021-12-12 17:09:34 +0100

    TUI window for inferior output

---
+++ gdb/tui/tui-output.c
@@ -0,0 +1,33 @@
+
+#include "defs.h"
+#include <tui/tui-output.h>
+
+#if GDB_MANAGED_TERMINALS
+
+#include <tui/tui-command.h>
+#include <tui/tui-io.h>
+#include <tui/tui-wingeneral.h>
+
+static std::vector<std::string> app_output;
+
+tui_output_window::tui_output_window ()
+     : tui_output_base_window(app_output)
+{
+  set_title (OUTPUT_NAME);
+}
+
+bool
+tui_output_write (const char *buf, long length_buf)
+{
+  if (buf != nullptr && length_buf > 0)
+    add_to_output (buf, length_buf, app_output);
+
+  if (!tui_is_window_visible (OUTPUT_WIN))
+    return false;
+
+  TUI_OUTPUT_WIN->refresh (true);
+
+  return true;
+}
+
+#endif
