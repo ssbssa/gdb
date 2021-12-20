fe6f83c1a8b502ecc0541f4944f1d93fda81f2d9
Hannes Domani <ssbssa@yahoo.de>
2021-12-20 17:04:15 +0100

    Fix enabling TUI the first time after terminal resizing

--- gdb/tui/tui.c
+++ gdb/tui/tui.c
@@ -468,6 +468,7 @@ tui_enable (void)
       tui_set_win_focus_to (tui_src_win ());
       keypad (tui_cmd_win ()->handle.get (), TRUE);
       wrefresh (tui_cmd_win ()->handle.get ());
+      tui_set_win_resized_to (false);
       tui_finish_init = false;
     }
   else
