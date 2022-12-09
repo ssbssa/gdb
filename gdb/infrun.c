60268edea507b6d2ef48a417084061dba1585dcb
Hannes Domani <ssbssa@yahoo.de>
2022-12-09 19:29:59 +0100

    Implement printing of return values of stepped-over functions
    Considering this example (gdb.base/return.c):
    
    (gdb) n
    44        printf("in main after func1\n");
    (gdb) n
    45        tmp2 = func2 ();
    (gdb) n
    46        tmp3 = func3 ();
    (gdb) n
    47        printf("exiting\n");
    (gdb) n
    48        return 0;
    
    When enabling the new functionality:
    
    (gdb) set print step-return-value on
    (gdb) n
    44        printf("in main after func1\n");
    (gdb) n
    45        tmp2 = func2 ();
    printf returned $1 = 20
    (gdb) n
    46        tmp3 = func3 ();
    func2 returned $2 = -5
    (gdb) n
    47        printf("exiting\n");
    func3 returned $3 = -5
    (gdb) n
    48        return 0;
    printf returned $4 = 8
    
    It shows the return values of all function which were stepped over
    with 'next', and puts them into convenience variables, similar to
    'finish'.

--- gdb/infrun.c
+++ gdb/infrun.c
@@ -6750,6 +6750,9 @@ process_event_stop_test (struct execution_control_state *ecs)
     case BPSTAT_WHAT_STEP_RESUME:
       infrun_debug_printf ("BPSTAT_WHAT_STEP_RESUME");
 
+      if (ecs->event_thread->thread_fsm () != nullptr)
+	ecs->event_thread->thread_fsm ()->capture_return_value ();
+
       delete_step_resume_breakpoint (ecs->event_thread);
       if (ecs->event_thread->control.proceed_to_finish
 	  && execution_direction == EXEC_REVERSE)
@@ -7115,7 +7118,12 @@ process_event_stop_test (struct execution_control_state *ecs)
 		}
 	    }
 	  else
-	    insert_step_resume_breakpoint_at_caller (frame);
+	    {
+	      insert_step_resume_breakpoint_at_caller (frame);
+
+	      if (ecs->event_thread->thread_fsm () != nullptr)
+		ecs->event_thread->thread_fsm ()->add_callee_info (frame);
+	    }
 
 	  keep_going (ecs);
 	  return;
