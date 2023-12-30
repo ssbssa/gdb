ffbfffff0fceeb34fc534d90c225187abf03c980
Hannes Domani <ssbssa@yahoo.de>
2023-12-30 13:22:27 +0100

    Use empty filename qualification in rbreak to match all files
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=25116

--- gdb/symtab.c
+++ gdb/symtab.c
@@ -5628,7 +5628,7 @@ rbreak_command (const char *regexp, int from_tty)
     }
 
   global_symbol_searcher spec (SEARCH_FUNCTION_DOMAIN, regexp);
-  if (file_name != nullptr)
+  if (file_name != nullptr && *file_name != 0)
     spec.add_filename (std::move (file_name));
   std::vector<symbol_search> symbols = spec.search ();
 
