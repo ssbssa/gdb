caba16aff93274f3db3513d325f9d6d266beb8d2
Hannes Domani <ssbssa@yahoo.de>
2024-01-06 17:21:46 +0100

    Fix crash when breakpoint condition causes inferior exit
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=16156

--- gdb/infcall.c
+++ gdb/infcall.c
@@ -903,7 +903,8 @@ run_inferior_call (std::unique_ptr<call_thread_fsm> sm,
      async_enable_stdin.  */
   if (current_ui->prompt_state == PROMPT_BLOCKED)
     {
-      if (call_thread->thread_fsm ()->finished_p ())
+      if (call_thread->thread_fsm ()
+	  && call_thread->thread_fsm ()->finished_p ())
 	async_disable_stdin ();
       else
 	async_enable_stdin ();
