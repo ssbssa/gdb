6f8b5eb48a656601ba217b00b09b7ad9404f283a
Tom Tromey <tom@tromey.com>
2025-04-28 18:52:32 +0200

    Fix int3 handling when stepping
    Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=31194

--- gdb/infrun.c
+++ gdb/infrun.c
@@ -7202,6 +7202,7 @@ handle_signal_stop (struct execution_control_state *ecs)
 
   /* Maybe this was a trap for a software breakpoint that has since
      been removed.  */
+  bool program_bp = false;
   if (random_signal && target_stopped_by_sw_breakpoint ())
     {
       if (gdbarch_program_breakpoint_here_p (gdbarch,
@@ -7210,6 +7211,7 @@ handle_signal_stop (struct execution_control_state *ecs)
 	  struct regcache *regcache;
 	  int decr_pc;
 
+	  program_bp = true;
 	  /* Re-adjust PC to what the program would see if GDB was not
 	     debugging it.  */
 	  regcache = get_thread_regcache (ecs->event_thread);
@@ -7248,7 +7250,8 @@ handle_signal_stop (struct execution_control_state *ecs)
   /* If not, perhaps stepping/nexting can.  */
   if (random_signal)
     random_signal = !(ecs->event_thread->stop_signal () == GDB_SIGNAL_TRAP
-		      && currently_stepping (ecs->event_thread));
+		      && currently_stepping (ecs->event_thread)
+		      && !program_bp);
 
   /* Perhaps the thread hit a single-step breakpoint of _another_
      thread.  Single-step breakpoints are transparent to the
